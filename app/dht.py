452ce8b59c5bb7e719e9191051a24dbb1f333777
Ryan Thomas Huff <ryanhuff@outlook.com>
2025-04-20 08:17:38 -0400

    Update dht.py
    Signed-off-by: Ryan Thomas Huff <ryanhuff@outlook.com>

--- app/dht.py
+++ app/dht.py
@@ -4,6 +4,7 @@ import logging
 import ssl
 import socket
 import os
+import json
 
 class DTLSServer(Server):
     def __init__(self):
@@ -13,12 +14,49 @@ class DTLSServer(Server):
         self.context = ssl.SSLContext(ssl.PROTOCOL_DTLS_SERVER)
         self.context.load_cert_chain(certfile=cert_path, keyfile=key_path)
         self.context.set_ciphers('ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384')
+        self.message_handlers = {
+            "pre_prepare": self.handle_pre_prepare,
+            "prepare": self.handle_prepare,
+            "commit": self.handle_commit
+        }
 
     async def listen(self, port):
         sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         sock = self.context.wrap_socket(sock, server_side=True)
         sock.bind(('0.0.0.0', port))
         await super().listen(port, socket=sock)
+        asyncio.create_task(self.handle_messages())
+
+    async def handle_messages(self):
+        while True:
+            for key in await self.get_keys("pre_prepare_*"):
+                message = await self.get(key)
+                if message:
+                    await self.message_handlers["pre_prepare"](json.loads(message))
+            for key in await self.get_keys("prepare_*"):
+                message = await self.get(key)
+                if message:
+                    await self.message_handlers["prepare"](json.loads(message))
+            for key in await self.get_keys("commit_*"):
+                message = await self.get(key)
+                if message:
+                    await self.message_handlers["commit"](json.loads(message))
+            await asyncio.sleep(1)
+
+    async def handle_pre_prepare(self, message):
+        from app.blockchain import Blockchain
+        blockchain = Blockchain()
+        await blockchain.handle_pre_prepare(message)
+
+    async def handle_prepare(self, message):
+        from app.blockchain import Blockchain
+        blockchain = Blockchain()
+        await blockchain.handle_prepare(message)
+
+    async def handle_commit(self, message):
+        from app.blockchain import Blockchain
+        blockchain = Blockchain()
+        await blockchain.handle_commit(message)
 
 async def discover_peers(bootstrap_nodes=None):
     server = DTLSServer()
